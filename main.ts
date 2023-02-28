b22e222a6a592fbc69c82ea4026b0ba305881a4c
jtamen <jl.tamen@gmail.com>
2023-02-28 11:27:07 +0100

    Mise à jour main.blocks, main.ts, Jeu-de-nim.md

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
+for (let index = 0; index <= 4; index++) {
+    strip.setPixelColor(index + 0, neopixel.colors(NeoPixelColors.Red))
+}
 basic.forever(function () {
-	
+    strip.show()
+    strip.showRainbow(1, 360)
 })
